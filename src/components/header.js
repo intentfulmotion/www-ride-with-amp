ebb2620680b24537459e8e7fa6bbd7dade0520db
Nishanth Samala <nishanth.samala@gmail.com>
2019-05-05 20:19:47 -0400

    fix autoplay and add poster while video is loading

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './style.scss';
 
 import Navbar from './navbar';
+import StoreButton from './storeButton'
+import ampPoster from '../images/poster.jpg'
 import ampVideoLoop from '../videos/amp-intro-loop.mp4'
 
 const Header = () => (
 	<section className="hero is-fullheight video">
 		<Navbar />
 		<div className="hero-video">
-			<video id="bgvid" playsInline="" muted="" autoPlay={true} loop>
+			<video id="bgvid" poster={ampPoster} playsInline muted autoPlay disableRemotePlayback loop>
 				<source src={ampVideoLoop} type="video/mp4" />
 			</video>
 		</div>
@@ -18,12 +20,10 @@ const Header = () => (
 				<h1 className="title">Smart running lights for your commute</h1>
 				<h2 className="subtitle">Beta Available Now.</h2>
 				<div className="buttons">
-					<a className="button is-white is-outlined">
+					<a className="button is-white is-outlined" href="#what">
 						<span>Learn More</span>
 					</a>
-					<a className="button is-primary">
-						<span>Get Yours</span>
-					</a>
+					<StoreButton />
 				</div>
 			</div>
 		</div>
