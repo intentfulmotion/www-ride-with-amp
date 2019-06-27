af4e05b5609266e3b97b5462b4ba0a8385bc9088
Nishanth Samala <nishanth.samala@gmail.com>
2019-06-27 14:14:03 -0400

    add publishable key

--- src/components/cart.js
+++ src/components/cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
 
   const redirectToCheckout = async (event) => {
     event.preventDefault()
-    const stripe = window.Stripe(process.env.STRIPE_PUBLISHABLE_KEY)
+    const stripe = window.Stripe(pk_live_DjVeCbarLJtrnDP5ntOs5Hua)
 
     const { error } = await stripe.redirectToCheckout({
       items: contents.map((item) => { return { sku: item[0], quantity: item[1] } }),
