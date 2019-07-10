80396eeccb8a743c4995fdd0aead80e7e7411254
Nishanth Samala <nishanth.samala@gmail.com>
2019-07-09 21:07:48 -0400

    preload fonts

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 
+import KiloGram from '../fronts/kilogram.otf'
+import NexaBold from '../fronts/nexa-bold.otf'
+import NexaLight from '../fronts/nexa-light.otf'
+
 export default () => (
 	<StaticQuery
 		query={graphql`
@@ -27,11 +31,13 @@ export default () => (
 				<title>{data.site.siteMetadata.title}</title>
 				<html lang="en" />
 				{/* Google / Search Engine Meta Tags */}
-				<meta itemprop="name" content={data.site.siteMetadata.author} /> />
+				<meta itemprop="name" content={data.site.siteMetadata.author} />
 				<meta
 					itemprop="description"
-					content={data.site.siteMetadata.description}
-				/>
+					content={data.site.siteMetadata.description} />
+				<link rel="preload" as="font" href={KiloGram} type="font/otf" crossOrigin="anonymous" />
+				<link rel="preload" as="font" href={NexaBold} type="font/otf" crossOrigin="anonymous" />
+				<link rel="preload" as="font" href={NexaLight} type="font/otf" crossOrigin="anonymous" />
 			</Helmet>
 		)}
 	/>
