520ccde05739fc195405ac835351582faa03a329
Nishanth Samala <nishanth.samala@gmail.com>
2020-01-31 16:09:11 -0600

    testing shipping rates

--- src-functions/rates/rates.js
+++ src-functions/rates/rates.js
@@ -1,13 +1,41 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
+let apiKey = process.env.FUNCTIONS_ENVIRONMENT === "production" ? process.env.SHIPPO_API_KEY_PRODUCTION : process.env.SHIPPO_API_KEY_STAGING
+const shippo = require(shippo)(apiKey)
+
+let fromAddress = JSON.parse(process.env.FROM_ADDRESS)
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+exports.handler = async (event, context, callback) => {
+  if(event.httpMethod !== 'POST' || !event.body) {
+    callback(null, {
+      statusCode: 400,
+      headers,
+    });
+  }
+
+  const data = JSON.parse(event.body)
+
+  if (!data.toAddress || !data.parcels) {
+    const error = `Missing to address or parcels`
+    callback(null, {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ error: error })
+    })
+  }
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    let shipment = await shippo.shipment.create({
+      "address_from": fromAddress,
+      "address_to": toAddress,
+      "parcels": [data.parcels],
+      "async": false
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: shipment.rates
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
