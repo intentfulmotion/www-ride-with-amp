95b9ce56181cea9aa98c5388fc7738b1bf08fe43
Nishanth Samala <nishanth.samala@gmail.com>
2020-02-16 12:27:02 -0500

    added checkout button to cart modal

--- src/components/cart-modal.js
+++ src/components/cart-modal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import CartSummary from './checkout/cart-summary'
 
 export default () => {
@@ -10,6 +11,9 @@ export default () => {
       <div className="modal-background" onClick={() => hideModal()}></div>
       <div className="modal-content box">
         <CartSummary />
+        <div className="has-text-centered">
+          <Link to="checkout" className="button is-primary has-text-white checkout-card">Checkout</Link>
+        </div>
       </div>
       <button className="modal-close is-large" aria-label="close" onClick={() => hideModal()}></button>
     </div>
