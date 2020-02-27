ad27a4daa7466919bb636758ae8b18e77573f3e9
Nishanth Samala <nishanth.samala@gmail.com>
2020-02-26 19:30:02 -0500

    show checkout modal on product detail pages. disable checkout button if there's nothing in the cart

--- src/components/cart-modal.js
+++ src/components/cart-modal.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from './cart-provider'
 import { Link } from 'gatsby'
 import CartSummary from './checkout/cart-summary'
 
 export default () => {
+  const { cart } = useContext(CartContext)
   const hideModal = () => {
     document.getElementById('cart-modal').classList.remove('is-active')
   }
+
+  let checkoutButton = cart.length > 0 ? 
+    (<Link to="checkout" className="button is-primary has-text-white checkout-card">Checkout</Link>) :
+    (<button className="button is-primary has-text-white checkout-card" disabled>Checkout</button>)
+
   return (
     <div id="cart-modal" className="modal">
       <div className="modal-background" onClick={() => hideModal()}></div>
       <div className="modal-content box">
         <CartSummary />
         <div className="has-text-centered">
-          <Link to="checkout" className="button is-primary has-text-white checkout-card">Checkout</Link>
+          { checkoutButton }
         </div>
       </div>
       <button className="modal-close is-large" aria-label="close" onClick={() => hideModal()}></button>
--- src/components/product-detail/index.js
+++ src/components/product-detail/index.js
@@ -5,9 +5,15 @@ import Carousel from '../carousel'
 export default ({ product }) => {
   const { add } = useContext(CartContext)
 
-  const addToCart = product.active ? (
+  const addToCart = (evt) => {
+    evt.preventDefault()
+    add(product.sku)
+    document.getElementById('cart-modal').classList.add('is-active')
+  }
+
+  const addToCartButton = product.active ? (
     <div>
-      <button className="button is-primary" onClick={() => { add(product.sku) }}>
+      <button className="button is-primary" onClick={(evt) => addToCart(evt)}>
         Add to Cart
       </button>
       <span className="subtitle is-4 product-price">${product.price}</span>
@@ -24,7 +30,7 @@ export default ({ product }) => {
         <div className="column">
           <h1 className='title has-text-black'>{product.name}</h1>
           <h2 className='subtitle'>{product.active ? "In stock" : "Out of stock"}</h2>
-          {addToCart}
+          {addToCartButton}
           <div className="content product-description"
             dangerouslySetInnerHTML={{
               __html: product.description.childMarkdownRemark.html,
